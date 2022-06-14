cbf0b9a46971cea980446e6e841c4067a0f7d3a9
Piotr Krzeminski <git@krzeminski.it>
2022-06-14 20:27:34 +0200

    Update distribution
    ```
    rm -rf dist/ && cp -R build/js dist
    ```

--- dist/packages/github-actions-typing/kotlin/github-actions-typing.js
+++ dist/packages/github-actions-typing/kotlin/github-actions-typing.js
@@ -12,10 +12,58 @@
 }(this, function (_, Kotlin) {
   'use strict';
   var println = Kotlin.kotlin.io.println_s8jyv4$;
+  var listOf = Kotlin.kotlin.collections.listOf_i5x0yv$;
+  var throwCCE = Kotlin.throwCCE;
+  var collectionSizeOrDefault = Kotlin.kotlin.collections.collectionSizeOrDefault_ba2ldo$;
+  var ArrayList_init = Kotlin.kotlin.collections.ArrayList_init_ww73n8$;
   function main() {
-    println('Hello from Node!');
+    println("Action's manifest:");
+    var manifest = readActionManifest();
+    println(manifest);
+  }
+  var fs;
+  function readActionManifest() {
+    var tmp$;
+    var $receiver = listOf(['yaml', 'yml']);
+    var destination = ArrayList_init(collectionSizeOrDefault($receiver, 10));
+    var tmp$_0;
+    tmp$_0 = $receiver.iterator();
+    while (tmp$_0.hasNext()) {
+      var item = tmp$_0.next();
+      destination.add_11rb$('action.' + item);
+    }
+    var firstOrNull$result;
+    firstOrNull$break: do {
+      var tmp$_1;
+      tmp$_1 = destination.iterator();
+      while (tmp$_1.hasNext()) {
+        var element = tmp$_1.next();
+        if (fileExists(element)) {
+          firstOrNull$result = element;
+          break firstOrNull$break;
+        }
+      }
+      firstOrNull$result = null;
+    }
+     while (false);
+    return (tmp$ = firstOrNull$result) != null ? readTextFileAsUtf8(tmp$) : null;
+  }
+  function fileExists(path) {
+    var tmp$;
+    return typeof (tmp$ = fs.existsSync(path)) === 'boolean' ? tmp$ : throwCCE();
+  }
+  function readTextFileAsUtf8(path) {
+    var tmp$;
+    return typeof (tmp$ = fs.readFileSync(path, 'utf8')) === 'string' ? tmp$ : throwCCE();
   }
   _.main = main;
+  Object.defineProperty(_, 'fs', {
+    get: function () {
+      return fs;
+    }
+  });
+  _.readActionManifest = readActionManifest;
+  fs = require('fs');
   main();
   Kotlin.defineModule('github-actions-typing', _);
   return _;
