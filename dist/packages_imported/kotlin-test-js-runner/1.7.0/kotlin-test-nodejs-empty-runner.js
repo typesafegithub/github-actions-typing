5c917d46acbd5c0ce8b587264f40525754d84885
Piotr Krzeminski <git@krzeminski.it>
2022-06-16 21:46:15 +0200

    Remove JS distribution

--- dist/packages_imported/kotlin-test-js-runner/1.7.0/kotlin-test-nodejs-empty-runner.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-"use strict";var t=function(){function t(){}return t.prototype.suite=function(t,n,o){},t.prototype.test=function(t,n,o){},t}();globalThis.kotlinTest={adapterTransformer:function(){return new t}};
-//# sourceMappingURL=kotlin-test-nodejs-empty-runner.js.map
