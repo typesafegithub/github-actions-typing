f207b4faeb318d147994d8abae6154636c35a899
Piotr Krzeminski <git@krzeminski.it>
2022-06-16 21:47:44 +0200

    Just build with GitHub

--- .github/workflows/build.main.kts
+++ .github/workflows/build.main.kts
@@ -9,7 +9,7 @@ import it.krzeminski.githubactions.dsl.workflow
 import it.krzeminski.githubactions.yaml.writeToFile
 
 workflow(
-    name = "Build and create distribution commit",
+    name = "Build",
     on = listOf(Push(branchesIgnore = listOf("main"))),
     sourceFile = __FILE__.toPath(),
 ) {
@@ -19,21 +19,5 @@ workflow(
     ) {
         uses(CheckoutV3())
         uses(GradleBuildActionV2(arguments = "build"))
-
-        run(
-            name = "Update contents of 'dist' directory",
-            command = "rm -rf dist/ && cp -R build/js dist",
-        )
-
-        run(
-            name = "Commit and push",
-            command = """
-                git config --global user.email "<>"
-                git config --global user.name "GitHub Actions Bot"
-                git add dist/
-                git commit -m "Update distribution for ${'$'}GITHUB_SHA"
-                git push
-            """.trimIndent()
-        )
     }
 }.writeToFile()
