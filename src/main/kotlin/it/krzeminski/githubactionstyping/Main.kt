f035f5f031c53d9679c14d2fa33d1c122e622058
Piotr Krzemiński <git@krzeminski.it>
2022-06-20 09:48:20 +0200

    Print plaintext report

--- src/main/kotlin/it/krzeminski/githubactionstyping/Main.kt
+++ src/main/kotlin/it/krzeminski/githubactionstyping/Main.kt
@@ -2,21 +2,23 @@ package it.krzeminski.githubactionstyping
 
 import it.krzeminski.githubactionstyping.parsing.parseManifest
 import it.krzeminski.githubactionstyping.parsing.readActionManifest
+import it.krzeminski.githubactionstyping.reporting.toPlaintextReport
 import it.krzeminski.githubactionstyping.validation.validate
 
 fun main() {
     println("Action's manifest:")
-    val manifest = readActionManifest()
+    val manifest = readActionManifest() ?: return
     println(manifest)
-
-    if (manifest == null) {
-        return
-    }
 
     val parsedManifest = parseManifest(manifest)
     println("Parsed manifest:")
     println(parsedManifest)
 
     val validationResult = parsedManifest.validate()
-    println(validationResult)
+
+    println()
+    println("==============================================")
+    println()
+
+    println(validationResult.toPlaintextReport())
 }
---
+++ src/main/kotlin/it/krzeminski/githubactionstyping/reporting/PlaintextReport.kt
@@ -0,0 +1,44 @@
+package it.krzeminski.githubactionstyping.reporting
+
+import it.krzeminski.githubactionstyping.validation.ActionValidationResult
+import it.krzeminski.githubactionstyping.validation.ItemValidationResult
+
+fun ActionValidationResult.toPlaintextReport(): String = buildString {
+    appendLine("Overall result: ")
+    this@toPlaintextReport.overallResult.appendStatus(this)
+    appendLine()
+
+    appendLine("Inputs:")
+    this@toPlaintextReport.inputs.forEach { (key, value) ->
+        appendLine("• $key:")
+        append("  ")
+        value.appendStatus(this)
+    }
+    if (this@toPlaintextReport.inputs.isEmpty()) {
+        appendLine("None.")
+    }
+    appendLine()
+
+    appendLine("Outputs:")
+    this@toPlaintextReport.outputs.forEach { (key, value) ->
+        appendLine("• $key:")
+        append("  ")
+        value.appendStatus(this)
+    }
+    if (this@toPlaintextReport.outputs.isEmpty()) {
+        appendLine("None.")
+    }
+    appendLine()
+}
+
+private fun ItemValidationResult.appendStatus(
+    stringBuilder: StringBuilder,
+) {
+    when (this) {
+        ItemValidationResult.Valid -> stringBuilder.appendLine(green("✔ VALID"))
+        is ItemValidationResult.Invalid -> stringBuilder.appendLine(red("❌ INVALID: ${this.message}"))
+    }
+}
+
+private fun green(text: String) = "\u001b[32m$text\u001b[0m"
+private fun red(text: String) = "\u001b[31m$text\u001b[0m"
