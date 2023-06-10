cf88c5668b0b8547e3a3d2facf447c6413647975
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2023-06-10 08:39:45 +0200

    chore: bump github-workflows-kt to 0.45.0 (#112)

--- .github/workflows/build.main.kts
+++ .github/workflows/build.main.kts
@@ -1,14 +1,14 @@
 #!/usr/bin/env kotlin
-@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.40.0")
+@file:DependsOn("io.github.typesafegithub:github-workflows-kt:0.45.0")
 
-import it.krzeminski.githubactions.actions.actions.CheckoutV3
-import it.krzeminski.githubactions.actions.gradle.GradleBuildActionV2
-import it.krzeminski.githubactions.actions.krzema12.GithubActionsTypingV1
-import it.krzeminski.githubactions.domain.RunnerType
-import it.krzeminski.githubactions.domain.triggers.PullRequest
-import it.krzeminski.githubactions.domain.triggers.Push
-import it.krzeminski.githubactions.dsl.workflow
-import it.krzeminski.githubactions.yaml.writeToFile
+import io.github.typesafegithub.workflows.actions.actions.CheckoutV3
+import io.github.typesafegithub.workflows.actions.gradle.GradleBuildActionV2
+import io.github.typesafegithub.workflows.actions.typesafegithub.GithubActionsTypingV1
+import io.github.typesafegithub.workflows.domain.RunnerType
+import io.github.typesafegithub.workflows.domain.triggers.PullRequest
+import io.github.typesafegithub.workflows.domain.triggers.Push
+import io.github.typesafegithub.workflows.dsl.workflow
+import io.github.typesafegithub.workflows.yaml.writeToFile
 
 workflow(
     name = "Build",
@@ -22,16 +22,16 @@ workflow(
         id = "build",
         runsOn = RunnerType.UbuntuLatest,
     ) {
-        uses(CheckoutV3())
-        uses(GradleBuildActionV2(arguments = "build"))
+        uses(action = CheckoutV3())
+        uses(action = GradleBuildActionV2(arguments = "build"))
     }
 
     job(
         id = "validate-types",
         runsOn = RunnerType.UbuntuLatest,
     ) {
-        uses(CheckoutV3())
-        uses(GithubActionsTypingV1())
+        uses(action = CheckoutV3())
+        uses(action = GithubActionsTypingV1())
     }
 
     job(
@@ -39,7 +39,7 @@ workflow(
         name = "Build Kotlin scripts",
         runsOn = RunnerType.UbuntuLatest,
     ) {
-        uses(CheckoutV3())
-        run("find -name '*.main.kts' | xargs kotlinc")
+        uses(action = CheckoutV3())
+        run(command = "find -name '*.main.kts' | xargs kotlinc")
     }
 }.writeToFile()
