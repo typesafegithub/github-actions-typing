9e0cc6c0c536f4be2fb61047b3ac0760e9010656
Björn Kautler <Bjoern@Kautler.net>
2025-01-27 06:45:23 +0100

    chore: migrate from K/JVM to KMP (#244)
    Part of #82.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,15 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode.FAIL_ON_PROJECT_REPOS
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.9.0")
+}
+
+dependencyResolutionManagement {
+    repositoriesMode = FAIL_ON_PROJECT_REPOS
+
+    repositories {
+        mavenCentral()
+    }
+}
+
 rootProject.name = "github-actions-typing"
